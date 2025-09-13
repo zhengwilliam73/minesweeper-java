ab1a384cc547606853cd2c0120be162e2fbb6d15
unknown <zhengwilliam73@gmail.com>
2025-09-13 18:26:11 -0400

    Added timer

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
+        game.startTimer();
     }
 }
--- Game.java
+++ Game.java
@@ -4,18 +4,31 @@ import javax.swing.*;
 public class Game {
     int rows = 10;
     int cols = 10;
+    Timer gameTimer;
+    int elapsedTime = 0;
     Grid[][] grid = new Grid[rows][cols];
 
     JFrame frame = new JFrame("Minesweeper");
     JPanel board = new JPanel();
+    JPanel timerPanel = new JPanel();
+    JLabel timerLabel = new JLabel("Seconds: 0");
 
         Game() {
             //Frame setup
-            frame.setSize(600, 600);
+            frame.setSize(600, 650);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());            
             frame.setLocationRelativeTo(null);
             
+
+            //Timer panel setup
+            timerPanel.setBackground(Color.BLACK);
+            timerLabel.setFont(new Font ("Consalas", Font.BOLD, 40));
+            timerLabel.setForeground(Color.LIGHT_GRAY);
+            timerPanel.add(timerLabel);
+            frame.add(timerPanel, BorderLayout.NORTH);
+
+
             //Board setup
             board.setLayout(new GridLayout(rows, cols));
             frame.add(board, BorderLayout.CENTER);
@@ -38,7 +51,25 @@ public class Game {
                     grid[i][j] = tile;
                 }
             }
+
+            //Timer setup
+            gameTimer = new Timer(1000, e -> {
+                elapsedTime++;
+                timerLabel.setText((String.format("Seconds: " + elapsedTime)));
+            });
             frame.setVisible(true);
         }
+
+        public void startTimer() {
+            elapsedTime = 0;
+            timerLabel.setText("Seconds: 0");
+            gameTimer.start();
+        }
+
+        public void stopTimer() {
+            gameTimer.stop();
+        }
+    
     }
 
+    
