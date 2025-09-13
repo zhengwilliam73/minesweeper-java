3f6fd0a98b6c465c25a437da8aec49e9f7d3c328
unknown <zhengwilliam73@gmail.com>
2025-09-13 14:50:04 -0400

    Added grids and tiles

--- Game.java
+++ Game.java
@@ -1,12 +1,44 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Game {
-    JFrame frame = new JFrame("Minesweeper");
+    int rows = 10;
+    int cols = 10;
+    Grid[][] grid = new Grid[rows][cols];
 
-    public Game() {
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    JFrame frame = new JFrame("Minesweeper");
+    JPanel board = new JPanel();
+
+        Game() {
+            //Frame setup
+            frame.setSize(600, 600);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new BorderLayout());            
+            frame.setLocationRelativeTo(null);
+            
+            //Board setup
+            board.setLayout(new GridLayout(rows, cols));
+            frame.add(board, BorderLayout.CENTER);
+    
+            //Grid setup
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    Grid tile = new Grid();
+                    tile.setXCoord(i);
+                    tile.setYCoord(j);
+    
+                    if ((i + j) % 2 == 0) {
+                        tile.setBackground(Color.LIGHT_GRAY);
+                    } 
+                    else {
+                        tile.setBackground(Color.GRAY);
+                    }
+    
+                    board.add(tile);
+                    grid[i][j] = tile;
+                }
+            }
+            frame.setVisible(true);
+        }
     }
-}
+
--- Grid.java
+++ Grid.java
@@ -1,3 +1,35 @@
-public class Grid {
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class Grid extends JButton {
+    private int x;
+    private int y;
+    private Color originalColor;
     
+    //Getters
+    public int getXCoord() {
+        return x;
+    }
+    public int getYCoord() {
+        return y;
+    }
+
+
+
+    // Setters
+    public void setXCoord(int x) {
+        this.x = x;
+    }
+
+    public void setYCoord(int y) {
+        this.y = y;
+    }
+    public void setColor(Color color) {
+        this.originalColor = color;
+        this.setBackground(this.originalColor);
+    }
+    public void setOriginalColor(Color color) {
+        this.setBackground(this.originalColor);
+    }
+
 }
