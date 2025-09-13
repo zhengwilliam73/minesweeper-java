d9d593912501e8d834b41c78f35488f0c63770f7
unknown <zhengwilliam73@gmail.com>
2025-09-13 13:37:44 -0400

    Popup window

--- Game.java
+++ Game.java
@@ -1,5 +1,12 @@
-import javax.swing.*;
-    
-    public class Game {
-        JFrame frame = new JFrame();
+import javax.swing.JFrame;
+
+public class Game {
+    JFrame frame = new JFrame("Minesweeper");
+
+    public Game() {
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
+}
